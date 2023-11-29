ca060cbbfbadf7f3fc9de12fcf4da5a0d1bd5790
ntb1911 <baonguyenthai580@gmail.com>
2023-11-30 00:19:30 +0700

    u

--- Model/Module/HDC.py
+++ Model/Module/HDC.py
@@ -6,72 +6,81 @@ from Model.Module.CBAM import *
 
 
 class DWConv(nn.Module):
-    def __init__(self, in_ch, out_ch, kernel_size, stride, dilation):
+    def __init__(self, in_ch, kernel_size, stride, dilation):
         super(DWConv, self).__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_ch, in_ch, kernel_size=1),
-            nn.BatchNorm2d(in_ch),
+            nn.Conv2d(in_ch, int(in_ch//4), kernel_size=1),
+            nn.BatchNorm2d(int(in_ch//4)),
             nn.ReLU(inplace=True)
         )
         self.dwconv = nn.Sequential(
-            SeparableConv2d(in_ch, out_ch, kernel_size=kernel_size, stride=stride, dilation=dilation, BatchNorm=nn.BatchNorm2d),
-            nn.BatchNorm2d(out_ch),
+            SeparableConv2d(int(in_ch//4), int(in_ch//4), kernel_size=kernel_size, stride=stride, dilation=dilation, BatchNorm=nn.BatchNorm2d),
+            nn.BatchNorm2d(int(in_ch//4)),
             nn.ReLU(inplace=True)
         )
         self.conv1_2 = nn.Sequential(
-            nn.Conv2d(out_ch, out_ch, kernel_size=1),
-            nn.BatchNorm2d(out_ch)
+            nn.Conv2d(int(in_ch//4), in_ch, kernel_size=1),
+            nn.BatchNorm2d(in_ch)
         )
-        self.ca = CBAM(in_ch=out_ch, no_spatial=True)
+        self.ca = CBAM(in_ch)
         self.relu = nn.ReLU(inplace=True)
     def forward(self, x):
+        x_tmp = x
         x = self.conv1(x)
-        x_tmp = self.dwconv(x)
-        x = self.conv1_2(x_tmp)
-        x = self.ca(x)
+        x = self.dwconv(x)
+        x = self.conv1_2(x)
+        x = self.ca(x) + x_tmp
+        x = self.relu(x)
         return x
 
 class SCBAM(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, rates):
+    def __init__(self, in_channels, kernel_size, stride, rates):
         super(SCBAM, self).__init__()
         self.dilated_conv = []
+        self.dwconv = nn.Sequential(
+            nn.Conv2d(in_channels, int(in_channels//4), kernel_size=3, padding=1, stride=1, dilation=1, bias=False),
+            nn.BatchNorm2d(in_channels//4),
+            nn.ReLU(inplace=True)
+        )
+
         for i, rate in enumerate(rates):
-            tmp = DWConv(in_channels, out_channels // len(rates), kernel_size, stride, rate)
+            tmp = DWConv(int(in_channels//4), kernel_size, stride, rate)
             self.dilated_conv.append(tmp)
         self.dilated_conv = nn.ModuleList(self.dilated_conv)
-        self.bn = nn.BatchNorm2d(out_channels)
+
+        self.bn = nn.BatchNorm2d(int(in_channels//4))
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
-        out = []
+        x = self.dwconv(x)
         for hdc in self.dilated_conv:
-            out.append(hdc(x))
-        combined_features = torch.cat(out, 1)
-        output = self.bn(combined_features)
+            x = hdc(x)*x
+
+        output = self.bn(x)
         output = self.relu(output)
         return output
 
 class HybridDilatedConv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, rates):
+    def __init__(self, in_channels, kernel_size, rates):
         super(HybridDilatedConv, self).__init__()
 
         effective_dilation = [rate * (kernel_size - 1) for rate in rates]
         p = [int((effective_dilation[i] - 1) / 2) for i in range(len(rates))]
-
+        channel = int(in_channels//4)
         self.dilated_conv = []
         for i, rate in enumerate(rates):
-            self.dilated_conv.append(nn.Conv2d(in_channels, out_channels // len(rates), kernel_size, padding=p[i]+1, dilation=rate, bias=False))
+            self.dilated_conv.append(nn.Conv2d(in_channels, channel, kernel_size, padding=p[i]+1, dilation=rate, bias=False))
+            in_channels = channel
         self.dilated_conv = nn.ModuleList(self.dilated_conv)
-        self.bn = nn.BatchNorm2d(out_channels)
+        self.bn = nn.BatchNorm2d(channel)
         self.relu = nn.LeakyReLU(inplace=True)
 
     def forward(self, x):
-        out = []
-        for hdc in self.dilated_conv:
-            out.append(hdc(x))
-        # for i in out:
-        #     print(i.size())
-        combined_features = torch.cat(out, 1)
-        output = self.bn(combined_features)
+        for i, hdc in enumerate(self.dilated_conv):
+            if i == 0:
+                x = hdc(x)
+            else:
+                x = hdc(x)*x
+        output = self.bn(x)
         output = self.relu(output)
         return output
